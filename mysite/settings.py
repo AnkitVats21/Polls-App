7d6e52d08fc676ef43c60bb1f275c317d8c1042f
AnkitVats21 <akmmfkh536@gmail.com>
2020-09-12 22:24:02 +0530

    basic working app created

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'td7ti0m9)k498$9vrqgetfk!2(f)u&tt!@!f1_wx_)r^%udlhe'
+SECRET_KEY = 'y#rts%xyf$hg306w%8xtvnv!cgc7#^w*a@u!z3qjxp8=!5t4lp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
 ]
 
 MIDDLEWARE = [
