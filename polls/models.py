dd4aede04f835700abe6159c5c54f4543c153152
AnkitVats21 <akmmfkh536@gmail.com>
2020-09-13 07:29:45 +0530

    final commit

--- polls/models.py
+++ polls/models.py
@@ -1,15 +1,28 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    #whenever we use this object it's gonna return the question text
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        # Fix bug after test because it should return False if its pub_date in the future
+        # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
+    #every choice we create corelates only with one question
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
